317ead6d06ff20861265b548f0db23354bb65c4b
Mohammad Waqas Zulfiqar Ali <www.mwaqas.com8@gmail.com>
2026-02-15 23:27:16 +0500

    All Certificates Added

--- src/data/certifcatesData.js
+++ src/data/certifcatesData.js
@@ -1,5 +1,8 @@
 import AI_Internship from "../assets/AI_Internship.jpg";
 import git from "../assets/Git_Course.jpg";
+import py_basic from "../assets/Python_HackerRank_Certificate.png";
+import Navtac_Course from "../assets/Navtac_AI_Course.jpg";
+import Azure_AI from "../assets/Azure_AI_Associate.jpg"; 
 export const certificatesData = [
     {
         id: 1,
@@ -10,9 +13,32 @@ export const certificatesData = [
     },
     {
         id: 2,
+        title: "Python AI/ ML 3 Months BootCamp",
+        issuer: " NAVTACC (Pakistan Government)",
+        date: "2024",
+        image: Navtac_Course,
+    },
+    {
+        id: 3,
+        title: "Azure AI Engineer Associate ",
+        issuer: " Azure",
+        date: "2024",
+        image: Azure_AI,
+
+
+    },
+    {
+        id: 4,
         title: "Git , GitLab, Github Fundamentals for Software Developer",
         issuer: "Udemy",
         date: "2025",
         image: git,
     },
+    {
+        id: 5,
+        title: "Python (Basic) Certificate Hacker Rank",
+        issuer: "HackerRank",
+        date: "2024",
+        image: py_basic
+    }
 ];
